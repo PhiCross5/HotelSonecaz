0efd5d0b8724318866fc46ec9c95697558c06612
PhiCross5 <noEmail>
2020-09-22 14:48:36 -0300

    commented JS code

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
-let navigatorState="OFF";
+
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
@@ -12,6 +12,10 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
   } 
 
+  //global variable navigatorState tracks wether the navigator is on display or hidden;
+  //this variable persists for the runtime of the web page.
+  let navigatorState="OFF";
+  //opens navigator if closed; closes navigator if open.
   function toggleNavigator(){
     if(navigatorState=="OFF"){
       openNav();
